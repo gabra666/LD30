56f6870d874a2b34603e761682007242ce335243
Bryan Garzon <gabra666@hotmail.com>
2014-08-23 20:19:04 +0100

    Character controller implemented Main Scene SkyScene VulcanScene ForestScene

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public int level;
+
+	private void OnCollisionEnter2D(Collision2D collision2d){
+		if(collision2d.gameObject.layer == LayerMask.NameToLayer("Player")){
+			Application.LoadLevel(level);
+		}
+	} 
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float move;
+	public float speed;
+
+	private bool grounded;
+	public Transform groundCheck;
+	private float groundRadius = 0.2f;
+	public LayerMask groundLayers;
+	public float jumpForce = 700f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		grounded = Physics2D.OverlapCircle(groundCheck.position,groundRadius,groundLayers);
+
+		if(grounded && Input.GetKeyDown(KeyCode.Space)){
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+		}
+		move = Input.GetAxis("Horizontal");
+		rigidbody2D.velocity = new Vector2(move * speed, rigidbody2D.velocity.y);
+		if(move > 0){
+
+		}
+		if(move < 0){
+
+		}
+	}
+}
